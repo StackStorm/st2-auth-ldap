e3ec81d5755c33c4289f2e1f9e3c9d82f417b8cd
Winson Chan <m4d.coder@gmail.com>
2016-02-29 22:24:35 +0000

    Add ActiveDirectory support
    Add support to search by other login name such as sAMAccountName and userPrincipalName. Restrict access to specified group DNs.

---
+++ tests/unit/test_active_directory.py
@@ -0,0 +1,59 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ldap
+import os
+import mock
+import unittest2
+import uuid
+
+from st2auth_enterprise_ldap_backend import ldap_backend
+
+
+LDAP_HOST = '127.0.0.1'
+LDAP_BIND_DN = 'cn=Administrator,cn=users,dc=stackstorm,dc=net'
+LDAP_BIND_PASSWORD = uuid.uuid4().hex
+LDAP_BASE_OU = 'dc=stackstorm,dc=net'
+LDAP_GROUP_DNS = ['cn=testers,dc=stackstorm,dc=net']
+LDAP_ID_ATTR = 'sAMAccountName'
+LDAP_USER_UID = 'jon'
+LDAP_USER_PASSWD = 'snow123'
+LDAP_USER_BAD_PASSWD = 'snow1234'
+
+LDAP_USER_SEARCH_RESULT = [('cn=Jon Snow,cn=users,dc=stackstorm,dc=net', [])]
+LDAP_GROUP_SEARCH_RESULT = [('cn=testers,dc=stackstorm,dc=net', [])]
+
+
+class ActiveDirectoryAuthenticationTest(unittest2.TestCase):
+
+    @mock.patch.object(
+        ldap.ldapobject.SimpleLDAPObject, 'simple_bind_s',
+        mock.MagicMock(return_value=None))
+    @mock.patch.object(
+        ldap.ldapobject.SimpleLDAPObject, 'search_s',
+        mock.MagicMock(side_effect=[LDAP_USER_SEARCH_RESULT, LDAP_GROUP_SEARCH_RESULT]))
+    def test_authenticate(self):
+        backend = ldap_backend.LDAPAuthenticationBackend(
+            LDAP_BIND_DN,
+            LDAP_BIND_PASSWORD,
+            LDAP_BASE_OU,
+            LDAP_GROUP_DNS,
+            LDAP_HOST,
+            id_attr=LDAP_ID_ATTR
+        )
+
+        authenticated = backend.authenticate(LDAP_USER_UID, LDAP_USER_PASSWD)
+
+        self.assertTrue(authenticated)
