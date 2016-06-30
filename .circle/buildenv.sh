ffc2e9d79d80b76dc06a6923eb79110b63a0493d
Kirill Izotov <enykeev@stackstorm.com>
2016-06-30 08:38:18 +0700

    Skip imports in __init__ file
    Also, determine paths relative to the current script

--- .circle/buildenv.sh
+++ .circle/buildenv.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+circle_dir="$(dirname "$0")"
+
+distros=($DISTROS)
+DISTRO=${distros[$CIRCLE_NODE_INDEX]}
+
 # Write export lines into ~/.buildenv and also source it in ~/.circlerc
 write_env() {
   for e in $*; do
@@ -11,20 +16,19 @@ write_env() {
 }
 
 fetch_version() {
-  if [ -f ../st2auth_enterprise_ldap_backend/__init__.py ]; then
+  cat
+  if [ -f ${circle_dir}/../st2auth_enterprise_ldap_backend/__init__.py ]; then
     # Get st2 version based on hardcoded string in st2common
-    # build takes place in `st2` repo
-    python -c 'execfile("../st2auth_enterprise_ldap_backend/__init__.py"); print __version__'
+    # build takes place in `st2-enterprise-auth-backend-ldap` repo
+    python -c "execfile(\"${circle_dir}/skip_import.py\"); execfile(\"${circle_dir}/../st2auth_enterprise_ldap_backend/__init__.py\"); print __version__"
   fi
 }
 
 PKG_VERSION=$(fetch_version)
 
-# for Bintray
-#ST2PKG_RELEASE=$(.circle/bintray.sh next-revision ${DISTRO}_staging ${ST2PKG_VERSION} st2)
 # for PackageCloud
 if [ -z "$CIRCLE_PR_REPONAME" ]; then
-  PKG_RELEASE=$(.circle/packagecloud.sh next-revision ${DISTRO} ${PKG_VERSION} st2-enterprise-auth-backend-ldap)
+  PKG_RELEASE=$(${circle_dir}/packagecloud.sh next-revision "${DISTRO}" "${PKG_VERSION}" st2-enterprise-auth-backend-ldap)
 else
   # is fork
   PKG_RELEASE=1
