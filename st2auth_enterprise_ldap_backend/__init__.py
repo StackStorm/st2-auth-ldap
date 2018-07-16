a1272641756d5008f4fdccea7408007182b146d9
stanley <stanley@stackstorm.com>
2018-07-16 11:01:54 +0000

    Update version to 2.8.1

--- st2auth_enterprise_ldap_backend/__init__.py
+++ st2auth_enterprise_ldap_backend/__init__.py
@@ -17,7 +17,7 @@ from __future__ import absolute_import
 
 from .ldap_backend import LDAPAuthenticationBackend
 
-__version__ = '2.8.0'
+__version__ = '2.8.1'
 
 __all__ = [
     'LDAPAuthenticationBackend'
