b726ba66956d4b4cacb82ad6b230d5fee0f51e21
Tomaz Muraus <tomaz@tomaz.me>
2019-05-07 17:05:21 +0200

    Update Copyright headers.

--- st2auth_enterprise_ldap_backend/ldap_backend.py
+++ st2auth_enterprise_ldap_backend/ldap_backend.py
@@ -1,4 +1,4 @@
-# Copyright (c) Extreme Networks, Inc - All Rights Reserved
+# Copyright (C) 2019 Extreme Networks, Inc - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential.
 # See the LICENSE file included with this work for details.
