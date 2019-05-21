3f1bdfd57a494754d9711a9023e3f16e18b97932
W Chan <m4d.coder@gmail.com>
2019-05-21 19:08:46 +0000

    Replace flake8 related dependencies with st2flake8
    Moving forward, st2flake8 on pypi will contain the requirements and custom plugins required for flake8 checks of st2 related repos. Update lint-configs subtree. Add st2flake8 to test-requirements.txt. Modify make lint and tox lint to use the .flake8-proprietary config file.

--- st2auth_enterprise_ldap_backend/__init__.py
+++ st2auth_enterprise_ldap_backend/__init__.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2019 Extreme Networks, Inc - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential.
-# See the LICENSE file included with this work for details.
+#
+# Unauthorized copying of this file, via any medium is strictly
+# prohibited. Proprietary and confidential. See the LICENSE file
+# included with this work for details.
 
 from __future__ import absolute_import
 
--- st2auth_enterprise_ldap_backend/ldap_backend.py
+++ st2auth_enterprise_ldap_backend/ldap_backend.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2019 Extreme Networks, Inc - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential.
-# See the LICENSE file included with this work for details.
+#
+# Unauthorized copying of this file, via any medium is strictly
+# prohibited. Proprietary and confidential. See the LICENSE file
+# included with this work for details.
 
 # pylint: disable=no-member
 
