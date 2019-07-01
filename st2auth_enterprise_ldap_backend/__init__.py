0fb1d29ad5608c29fed0a1fff1ab5348839a8973
stanley <stanley@stackstorm.com>
2019-07-01 22:52:02 +0000

    Update version to 3.2dev

--- st2auth_enterprise_ldap_backend/__init__.py
+++ st2auth_enterprise_ldap_backend/__init__.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import
 
 from .ldap_backend import LDAPAuthenticationBackend
 
-__version__ = '3.1dev'
+__version__ = '3.2dev'
 
 __all__ = [
     'LDAPAuthenticationBackend'
