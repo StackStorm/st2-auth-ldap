8aa050265bc97a3cb3026a0e0803f56b762d7aed
W Chan <m4d.coder@gmail.com>
2020-05-14 19:20:10 +0000

    Update the year in the copyright headers

--- dist_utils.py
+++ dist_utils.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019 Extreme Networks, Inc - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential.
-# See the LICENSE file included with this work for details.
+# Copyright (C) 2020 Extreme Networks, Inc - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium is strictly
+# prohibited. Proprietary and confidential. See the LICENSE file
+# included with this work for details.
 
 from __future__ import absolute_import
 
