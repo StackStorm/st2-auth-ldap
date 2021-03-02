a284208eafe7e64c08f1a374b8060f4976d9bcd7
stanley <stanley@stackstorm.com>
2021-03-02 22:31:41 +0000

    Update version info for release - 3.4.0

--- st2auth_ldap/__init__.py
+++ st2auth_ldap/__init__.py
@@ -17,7 +17,7 @@ from __future__ import absolute_import
 
 from .ldap_backend import LDAPAuthenticationBackend
 
-__version__ = '3.4dev'
+__version__ = '3.4.0'
 
 __all__ = [
     'LDAPAuthenticationBackend'
