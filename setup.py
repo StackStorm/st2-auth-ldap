0c0174c480d6e1c9e49835c7fcd07268d7904e8f
Jacob Floyd <cognifloyd@gmail.com>
2024-10-20 20:00:40 -0500

    add description for pypi

--- setup.py
+++ setup.py
@@ -27,10 +27,15 @@ INIT_FILE = os.path.join(BASE_DIR, 'st2auth_ldap', '__init__.py')
 version = parse_version_string(INIT_FILE)
 install_reqs, dep_links = fetch_requirements(REQUIREMENTS_FILE)
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='st2-auth-ldap',
     version=version,
     description='StackStorm authentication backend for LDAP.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='StackStorm, Inc.',
     author_email='info@stackstorm.com',
     url='https://stackstorm.com/',
